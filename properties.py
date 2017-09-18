d954126ba461d60c680891fc88577f85adc919df
Siri Gowda <sirigowd@usc.edu>
2017-09-18 14:55:37 -0700

    Update properties.py

--- properties.py
+++ properties.py
@@ -4,10 +4,10 @@ access_token_secret = "#ENTER YOUR ACCESS TOKEN SECRET#"
 consumer_key = "#ENTER YOUR CONSUMER KEY#"
 consumer_secret = "#ENTER YOUR CONSUMER SECRET#"
 
-input_file_path= 'collectTweets.csv'
+input_file_path = 'collectTweets.csv'
 
-training_data='/twitterdata/train.csv'
-test_data='/twitterdata/test.csv'
-np_test_results='/twitterdata/np_results.csv'
+training_data = '/twitterdata/train.csv'
+test_data = '/twitterdata/test.csv'
+np_test_results = '/twitterdata/np_results.csv'
 
-keywords =["losangeles", "newyork"]
+keywords = [ "losangeles" , "newyork" ]
