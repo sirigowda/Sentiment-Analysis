9923c9cf9c75b89591941db2b0d6c929340dc08a
Siri Gowda <sirigowd@usc.edu>
2017-09-18 15:09:32 -0700

    Update NaiveBayes.py

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -4,15 +4,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import TfidfTransformer
 import properties
 
-<<<<<<< HEAD
-## Training set contains 500000 positive, negative and neutral tweets each (total 1500000 tweets)
-f = pd.read_csv(properties.training_data, sep=',', names=['text', 'sentiment'], dtype=str)
-=======
 # Training set contains 500000 positive, negative and neutral tweets each (total 1500000 tweets)
 f = pd.read_csv( properties.training_data, sep=',', names=['text','sentiment'], dtype=str)
->>>>>>> d954126ba461d60c680891fc88577f85adc919df
 print f.head()
-
 
 def split_into_lemmas(tweet):
     bigram_vectorizer = CountVectorizer(ngram_range=(1, 3), token_pattern=r'\b\w+\b', min_df=1, encoding='utf-8',
@@ -30,13 +24,9 @@ tfidf_transformer = TfidfTransformer().fit(text_bow)
 
 text_tfidf = tfidf_transformer.transform(text_bow)
 
-<<<<<<< HEAD
-# test file contains the test test of 498 tweets
-tweets = pd.read_csv(properties.test_data, sep=',', names=['Text'], dtype=str)
-=======
 # Test file contains the test set of 498 tweets
 tweets = pd.read_csv( properties.test_data, sep=',', names=['Text'], dtype=str)
->>>>>>> d954126ba461d60c680891fc88577f85adc919df
+
 classifier_nb = MultinomialNB(class_prior=None).fit(text_tfidf, f['sentiment'])
 
 sentiments = pd.DataFrame(columns=['text', 'class', 'prob'])
@@ -57,10 +47,5 @@ for _, tweet in tweets.iterrows():
         print e
         sentiments.loc[i - 1, 'text'] = tweet.values[0]
 
-<<<<<<< HEAD
-sentiments.to_csv(properties.naivebayes_results, encoding='utf-8')
-# print sentiments
-=======
 sentiments.to_csv( properties.np_test_results, encoding ='utf-8')
-#print sentiments
->>>>>>> d954126ba461d60c680891fc88577f85adc919df
+print sentiments
