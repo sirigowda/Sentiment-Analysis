444ec66fed2d5a77790534f00ebb8447c39f1e02
Siri Gowda <sirigowd@usc.edu>
2017-09-18 22:47:55 -0700

    Update properties.py

--- ExtractTweets/properties.py
+++ ExtractTweets/properties.py
@@ -5,10 +5,4 @@ consumer_secret = "#ENTER YOUR CONSUMER SECRET#"
 
 input_file_path = 'collectTweets.csv'
 
-training_data = '/twitterdata/train.csv'
-test_data = '/twitterdata/test.csv'
-
-naivebayes_results = '/twitterdata/naivebayes_results.csv'
-onevsrest_results = '/twitterdata/onevsrest_results.csv'
-
 keywords = ["losangeles", "newyork"]
