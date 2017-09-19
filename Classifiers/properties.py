75a93feaef3ccf38240a19526bc6a3a7e79389a0
Siri Gowda <sirigowd@usc.edu>
2017-09-18 22:51:16 -0700

    Update properties.py

--- Classifiers/properties.py
+++ Classifiers/properties.py
@@ -1,14 +1,5 @@
-access_token = "#ENTER YOUR ACCESS TOKEN#"
-access_token_secret = "#ENTER YOUR ACCESS TOKEN SECRET#"
-consumer_key = "#ENTER YOUR CONSUMER KEY#"
-consumer_secret = "#ENTER YOUR CONSUMER SECRET#"
-
-input_file_path = 'collectTweets.csv'
-
 training_data = '/twitterdata/train.csv'
 test_data = '/twitterdata/test.csv'
 
 naivebayes_results = '/twitterdata/naivebayes_results.csv'
 onevsrest_results = '/twitterdata/onevsrest_results.csv'
-
-keywords = ["losangeles", "newyork"]
