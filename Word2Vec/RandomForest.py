90b67b09205f42b87fad44058d66b8c103209176
Siri Gowda <sirigowd@usc.edu>
2017-09-20 01:31:38 -0700

    Update RandomForest.py

--- Word2Vec/RandomForest.py
+++ Word2Vec/RandomForest.py
@@ -1,10 +1,10 @@
-# Fit a random forest to the training data, using 100 trees
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
 import externalproperties
 
+# Fit a random forest to the training data, return predicted results
 def classify_tweets(X_train, y_train, X_test):
-    forest = RandomForestClassifier( n_estimators = 10,max_depth=10 )
+    forest = RandomForestClassifier( n_estimators = 100, max_depth=10 )
 
     print "Fitting a random forest to labeled training data..."
     forest = forest.fit(X_train,y_train)
@@ -12,6 +12,5 @@ def classify_tweets(X_train, y_train, X_test):
     print "Predicting sentiments for test data..."
     result = forest.predict( X_test )
 
-    # Write the test results to output file
     output = pd.DataFrame( data={"text":X_test["Text"][0], "sentiment": result} )
     return output
