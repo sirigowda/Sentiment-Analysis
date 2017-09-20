92b99771e01b1d1c6c758d6b9b3f43febd70f236
Siri Gowda <sirigowd@usc.edu>
2017-09-20 01:34:06 -0700

    Update properties.py

--- ExtractTweets/properties.py
+++ ExtractTweets/properties.py
@@ -3,6 +3,6 @@ access_token_secret = "#ENTER YOUR ACCESS TOKEN SECRET#"
 consumer_key = "#ENTER YOUR CONSUMER KEY#"
 consumer_secret = "#ENTER YOUR CONSUMER SECRET#"
 
-input_file_path = 'collectTweets.csv'
+output_file_path = 'collectTweets.csv'
 
 keywords = ["losangeles", "newyork"]
